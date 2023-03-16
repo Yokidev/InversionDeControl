2f0a8b8077a7254f8982cbdd853d07f89c9e83ab
Jose <->
2023-03-16 09:06:00 +0100

    Practica 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,9 +10,11 @@ public class Main {
 
         EntrenadorFutbol entrenadorF = (EntrenadorFutbol) context.getBean("entrenadorF");
         EntrenadorBaloncesto entrenadorB = (EntrenadorBaloncesto) context.getBean("entrenadorB");
+        EntrenadorTenis entrenadorT = (EntrenadorTenis) context.getBean("entrenadorT");
 
         System.out.println(entrenadorF.Entrenamiento());
         System.out.println(entrenadorB.Entrenamiento());
+        System.out.println(entrenadorT.Entrenamiento());
 
     }
 }
