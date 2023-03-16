e6ef3c007c940e736fc78c4cc521f6f084818154
Jose <->
2023-03-16 09:07:15 +0100

    Practica 5

---
+++ src/main/java/org/example/EntrenadorTenis.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class EntrenadorTenis implements Entrenador {
+    @Override
+    public String Entrenamiento() {
+        return "Hacer 50 saques directos";
+    }
+}
